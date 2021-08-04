0e540fde73bc76a80ea91ea2975f476aa4d1a7bc
ru12321 <1075348489@qq.com>
2021-08-04 10:15:50 +0800

    Socket版本使用ThreadPoolFactory线程池

--- rpc-core/src/main/java/com/mrru/socket/server/SocketServer.java
+++ rpc-core/src/main/java/com/mrru/socket/server/SocketServer.java
@@ -6,6 +6,7 @@ import com.mrru.exception.RpcException;
 import com.mrru.registry.ServiceRegistry;
 import com.mrru.RequestHandler;
 import com.mrru.serializer.CommonSerializer;
+import com.mrru.util.ThreadPoolFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,11 +26,6 @@ public class SocketServer implements RpcServer
 {
     private static final Logger logger = LoggerFactory.getLogger(SocketServer.class);
 
-    private static final int CORE_POOL_SIZE = 5;
-    private static final int MAXIMUM_POOL_SIZE = 50;
-    private static final int KEEP_ALIVE_TIME = 60;
-    private static final int BLOCKING_QUEUE_CAPACITY = 100;
-
     private final ExecutorService threadPool;
     private final ServiceRegistry serviceRegistry;
     private CommonSerializer serializer;
@@ -41,11 +37,8 @@ public class SocketServer implements RpcServer
     public SocketServer(ServiceRegistry serviceRegistry)
     {
         this.serviceRegistry = serviceRegistry;
-        BlockingQueue<Runnable> workingQueue = new ArrayBlockingQueue<>(BLOCKING_QUEUE_CAPACITY);
-        ThreadFactory threadFactory = Executors.defaultThreadFactory();
-        threadPool = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_TIME, TimeUnit.SECONDS, workingQueue, threadFactory);
+        threadPool = ThreadPoolFactory.createDefaultThreadPool("socket-rpc-server");
     }
-
 
     //注册实现类 并立即开始监听
     //service 服务端真正的 实现类
